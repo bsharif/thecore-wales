13d9376802d341bb0b8dd2b5f8dbdb0b86d7733c
Ben Sharif <ben.sharif@live.com>
2015-07-01 18:55:04 +0100

    Happy to contact removed temporarily

--- models/db_blog.py
+++ models/db_blog.py
@@ -20,5 +20,5 @@ db.define_table('reg_questionnaire',
                 Field('part_time_training','text'),
                 Field('teaching_research','text'),
                 Field('advice','text'),
-                Field('happy_to_contact','text'),
+#                 Field('happy_to_contact','text'),
                 auth.signature)
