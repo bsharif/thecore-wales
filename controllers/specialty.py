c0d247534bf27d948ad0d386dea204c50678657e
Ben Sharif <ben.sharif@live.com>
2015-07-02 18:03:04 +0100

    changed form labels to function

--- controllers/specialty.py
+++ controllers/specialty.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+def get_form_labels():
+    form_labels = {'pursure_a_career':'What made you decide to pursue a career in this specialty?','rotation_useful':'What rotations did you do in your training and were any particularly helpful?','enjoy_most':'What do you most enjoy about the specialty?','challenging_aspects':'Are there any aspects of the specialty you find challenging/ frustrating?','typical_patients':'Are there typical patient groups and if so what are they like?','typical_day':'What does the typical day in the life of a  registrar for this specialty involve?','typical_hours':'What are the typical working hours?','part_time_training':'Is there any opportunity for part time training?','teaching_research':'Are there many opportunities for teaching/research/managerial positions within the specialty?','advice':'Do you have any advice for someone considering a career in this specialty?','happy_to_contact':'Would you be happy to be contacted by email by trainees who had any further questions?'}
+    return form_labels
+
 def index(): 
     specialty_dict = {} 
     specialty_records = db(db.reg_questionnaire).select()
@@ -25,7 +29,7 @@ def view():
 
 @auth.requires_membership('administrator')
 def edit_post():
-    form_labels = {'pursure_a_career':'What made you decide to pursue a career in this specialty?','rotation_useful':'What rotations did you do in your training and were any particularly helpful?','enjoy_most':'What do you most enjoy about the specialty?','challenging_aspects':'Are there any aspects of the specialty you find challenging/ frustrating?','typical_patients':'Are there typical patient groups and if so what are they like?','typical_day':'What does the typical day in the life of a  registrar for this specialty involve?','typical_hours':'What are the typical working hours?','part_time_training':'Is there any opportunity for part time training?','teaching_research':'Are there many opportunities for teaching/research/managerial positions within the specialty?','advice':'Do you have any advice for someone considering a career in this specialty?','happy_to_contact':'Would you be happy to be contacted by email by trainees who had any further questions?'}
+    form_labels = get_form_labels()
     post_id = request.vars.post_id
     form=SQLFORM(db.reg_questionnaire, post_id, 
                  showid=False,
@@ -36,8 +40,7 @@ def edit_post():
     return locals()
 
 def add_new():
-    form_labels = {'pursure_a_career':'What made you decide to pursue a career in this specialty?','rotation_useful':'What rotations did you do in your training and were any particularly helpful?','enjoy_most':'What do you most enjoy about the specialty?','challenging_aspects':'Are there any aspects of the specialty you find challenging/ frustrating?','typical_patients':'Are there typical patient groups and if so what are they like?','typical_day':'What does the typical day in the life of a  registrar for this specialty involve?','typical_hours':'What are the typical working hours?','part_time_training':'Is there any opportunity for part time training?','teaching_research':'Are there many opportunities for teaching/research/managerial positions within the specialty?','advice':'Do you have any advice for someone considering a career in this specialty?','happy_to_contact':'Would you be happy to be contacted by email by trainees who had any further questions?'}
-    
+    form_labels = get_form_labels()
     form = SQLFORM(db.reg_questionnaire,
                   labels=form_labels)
     if form.process().accepted:
